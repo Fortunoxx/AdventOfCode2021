bed005fc9b8fcef83e55c5f28cdafdfffa8e60f3
Rob Gates <robert_goetz@hotmail.com>
2021-12-08 00:06:20 +0100

    day04

---
+++ src/day04/code.py
@@ -0,0 +1,142 @@
+day = "04"
+files = [
+    {"key": "input", "file": f"src/day{day}/input.dat"},
+    {"key": "sample", "file": f"src/day{day}/sample.dat"}
+]
+
+
+def getFieldFromLineNumber(lineNumber):
+    if lineNumber >= 2:
+        return (lineNumber - 2) % 5
+    return 0
+
+
+def convert(fileInfo, blockSize=6):
+    with open(fileInfo["file"]) as file:
+        id = 0
+        isFirst = True
+        lineCounter = 0
+        numbers = []
+        sets = []
+        rows = []
+        columns = []
+
+        for line in file:
+            line = line.replace("\n", "").replace("  ", " ").strip()
+
+            if isFirst:
+                isFirst = False
+                numbers = line.split(",")
+            elif line != "":
+                rows.append(line.split(" "))
+
+            if lineCounter > 0 and lineCounter % blockSize == 0:
+                size = len(rows)
+                i = 0
+                while i < size:
+                    col = []
+                    for a in rows[len(rows)-(blockSize-1):]:
+                        col.append(a[i])
+                    columns.append(col)
+                    i += 1
+
+                sets.append({"id": id, "rows": rows, "columns": columns})
+                id += 1
+                rows = []
+                columns = []
+            lineCounter += 1
+
+    return {"numbers": numbers, "sets": sets}
+
+
+def findWinningBoard(converted, amount=5):
+    drawnNumbers = []
+    for number in converted["numbers"]:
+        drawnNumbers.append(number)
+        if(len(drawnNumbers) < amount):
+            continue
+
+        for item in converted["sets"]:
+            for row in item["rows"]:
+                if findMatches(drawnNumbers, row):
+                    return (item, drawnNumbers)
+            for col in item["columns"]:
+                if findMatches(drawnNumbers, col):
+                    return (item, drawnNumbers)
+    return (None, None)
+
+
+def findLastBoard(converted, amount=5):
+    drawnNumbers = []
+    winningOrder = []
+
+    for number in converted["numbers"]:
+        drawnNumbers.append(number)
+        if(len(drawnNumbers) < amount):
+            continue
+
+        for item in converted["sets"]:
+            if item["id"] in winningOrder: 
+                continue
+
+            for row in item["rows"]:
+                if findMatches(drawnNumbers, row):
+                    winningOrder.append(item["id"])
+                    break
+            for col in item["columns"]:
+                if findMatches(drawnNumbers, col):
+                    winningOrder.append(item["id"])
+                    break
+            
+            if (len(winningOrder) == len(converted["sets"])):
+                return (item, drawnNumbers)
+
+    return None
+
+
+def findMatches(numbers, arrayToCheck, amount=5):
+    matches = 0
+    for n in numbers:
+        for item in arrayToCheck:
+            if n == item:
+                matches += 1
+                if matches >= amount:
+                    return True
+    return False
+
+
+def calculateSum(item, numbers):
+    if item is None:
+        return -1
+
+    leftOvers = []
+    for row in item["rows"]:
+        for r in row:
+            if not r in numbers:
+                leftOvers.append(r)
+
+    theSum = sum(list(map(int, leftOvers)))
+    theLast = int(numbers[-1])
+    return theSum * theLast
+
+
+def process(fileInfos):
+    for fileInfo in fileInfos:
+        converted = convert(fileInfo)
+        results = findWinningBoard(converted)
+        theResult = calculateSum(results[0], results[1])
+        result = {"file": fileInfo['key'], "theResult": theResult}
+        print(f"Part I: {result}")
+
+
+def process2(fileInfos):
+    for fileInfo in fileInfos:
+        converted = convert(fileInfo)
+        results = findLastBoard(converted)
+        theResult = calculateSum(results[0], results[1])
+        result = {"file": fileInfo['key'], "theResult": theResult}
+        print(f"Part II: {result}")
+
+
+process(files)
+process2(files)
