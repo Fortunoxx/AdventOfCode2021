c298704471e894db4be4f58e1fdd1f7c1886c4ab
Rob Gates <robert_goetz@hotmail.com>
2021-12-09 21:31:28 +0100

    check for existing file

--- src/puzzle/puzzle.py
+++ src/puzzle/puzzle.py
@@ -1,9 +1,12 @@
 import requests
+import os
 
 def FetchForDay(day):
     with open('src/puzzle/cookie.txt') as cookieFile:
         for cookie in cookieFile:
             cookies = dict(session=cookie)
             input = requests.get(f'https://adventofcode.com/2021/day/{int(day)}/input', cookies=cookies)
-            with open(f'src/day{day}/input.dat', 'w') as inputFile:
-                inputFile.write(input.text)
+            filename = f'src/day{day}/input.dat'
+            if not os.path.exists(filename):
+                with open(filename, 'x') as inputFile:
+                    inputFile.write(input.text)
